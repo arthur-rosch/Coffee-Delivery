928ea233167cab9f334f24199690b453a3ede644
Arthur Rosch <rosch.arthur@gmail.com>
2023-01-06 18:37:08 -0300

    Projeto Configurado, Iniciando Projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const DefaultTheme = {
+  white: '#FFFFFF',
+  yellow: '',
+  'yellow-dark': '',
+  'yellow-light': '',
+
+  purple: '',
+  'purple-light': '',
+  'purple-dark': '',
+
+  'base-title': '',
+  'base-subtitle': '',
+  'base-text': '',
+  'base-label': '',
+  'base-hover': '',
+  'base-button': '',
+  'base-input': '',
+  'base-card': '',
+  background: '',
+}
